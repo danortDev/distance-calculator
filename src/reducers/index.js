9584e5100ea79ffba67ab165453ed5775115276e
Daniel Ortiz <daniel.ortiz@blanclink.com>
2016-08-12 15:37:18 -0400

    add basic reducer file with thunk middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-// Set up your root reducer here...
- import { combineReducers } from 'redux';
- export default combineReducers;
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import {syncHistoryWithStore, routerReducer as routing} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+import thunk from 'redux-thunk';
+import initialState from '../constants/initialState';
+import {reducer as form} from 'redux-form';
+
+const reducers = {
+    form,
+    routing
+};
+
+const reducer = combineReducers(reducers);
+const store = createStore(
+    reducer,
+    initialState,
+    compose(applyMiddleware(thunk))
+);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+export default store;
