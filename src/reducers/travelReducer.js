a5de62c5a48cab95df61ebba306f6b8081717737
Daniel Ortiz <daniel.ortiz@blanclink.com>
2016-08-13 18:05:36 -0400

    add result to travel reducer

--- src/reducers/travelReducer.js
+++ src/reducers/travelReducer.js
@@ -3,10 +3,19 @@ import initialState from '../constants/initialState';
 
 const travelReducer = function(state = initialState.travel, action){
     switch (action.type) {
+        case types.SET_TRAVEL:
+            return action.travel;
         case types.SET_ORIGIN:
             return Object.assign({}, state, {origin: action.origin});
         case types.SET_DESTINATION:
             return Object.assign({}, state, {destination: action.destination});
+        case types.SET_RESULTS:
+            return Object.assign({}, state, {results: action.results});
+        case types.ADD_RESULTS:
+            return Object.assign({}, state, {results: [
+                ...state.results,
+                ...action.results
+            ]});
         default:
             return state;
     }
