8fe88878ad1c64544ec77266f61c71e11ea9073d
Daniel Ortiz <daniel.ortiz@blanclink.com>
2016-08-14 16:18:18 -0400

    fix logs container and logs row components

--- src/components/LogsContainer.js
+++ src/components/LogsContainer.js
@@ -1,14 +1,22 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import LogRow from './LogRow';
 
 const setLog = logs =>
     logs.map( (log, index) =>
-        
+        <LogRow key={index} {...log} />
     );
 
 const LogsContainer = ({logs}) => (
-    <div>Log container</div>
+    <div>
+        {logs.length > 0 && <LogRow action="Action" at="Ocurred At" />}
+        {setLog(logs)}
+    </div>
 );
+
+LogsContainer.propTypes = {
+    logs: PropTypes.array.isRequired
+};
 
 const mapStateToProps = state => ({
     logs: state.logs
